c4e3cdc71787b899a3cfc65e9cfb7500fff2c5b7
TamTobiRalph <ralphtamtobi@hotmail.com>
2022-07-06 18:07:15 +0100

    first commit

---
+++ # INSTALLING NECESSARY PACKAGES.r
@@ -0,0 +1,23 @@
+# INSTALLING NECESSARY PACKAGES
+
+# install.packages("tidyverse")
+
+library()
+
+# The Next packages are Machine Learning Packages
+# install.packages("mlr3")
+# install.packages("caret")
+
+#' knitr (perhaps a play on “neater”) is a package that automates report generation. 
+#' Typically, you’d write a statistical report by copying and pasting the relevant results of your 
+#' analysis into a word processor and typing in additional information about these results. 
+#' This process is error-prone due to the amount of manual work and the multitasking between analysis
+#' and writing.
+
+#' knitr dynamically generates reports from R code by combining chunks of code with their corresponding 
+#' descriptions. This way, whenever you compile a document, any changes made to the R code will reflect in 
+#' the corresponding section in the report. knitr is especially useful if you’re a researcher or need to 
+#' generate reports of your data analyses. It can integrate your R code into many formats such as Markdown 
+#' and LaTeX, or even create a report in HTML. 
+
+# install.packages("knitr")
